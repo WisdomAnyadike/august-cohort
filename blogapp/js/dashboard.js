b264cd840d0428c8cfc719400888d040c5330efe
WisdomAnyadike <anyadikewisdom@icloud.com>
2025-09-09 12:37:50 +0100

    new

--- blogapp/js/dashboard.js
+++ blogapp/js/dashboard.js
@@ -1,5 +1,6 @@
 let userDatabase = JSON.parse(localStorage.getItem('augustJsDatabase')) || []
 
+
 let currentUserIndex = Number(localStorage.getItem('augustJsUserIndex'))
 
 let currentUserObj = userDatabase[currentUserIndex]
@@ -7,7 +8,87 @@ let currentUserObj = userDatabase[currentUserIndex]
 
 console.log(currentUserIndex);
 console.log(currentUserObj);
+let blogDatabase = JSON.parse(localStorage.getItem('blogDatabase')) || []
 
+displayBlog()
 
 let currentUser = document.getElementById('currentUser')
 currentUser.innerHTML = currentUserObj.username
+function postBlog() {
+    if (blogtitle.value.trim() === '' || blogdescription.value.trim() === '') {
+        alert('All fields are mandaratory')
+    }
+    else {
+        let blog = {
+            blogdescription: blogdescription.value.trim(),
+            blogtitle: blogtitle.value.trim()
+        }
+        blogDatabase.push(blog)
+        localStorage.setItem('blogDatabase', JSON.stringify(blogDatabase))
+        displayBlog()
+    }
+
+
+}
+
+
+function displayBlog() {
+    let BlogPreview = document.getElementById('blogPreview')
+    BlogPreview.innerHTML = ""
+
+
+    for (let index = 0; index < blogDatabase.length; index++) {
+        const element = blogDatabase[index];
+
+
+        BlogPreview.innerHTML += ` <div id="blogCard">
+        <p id="forBlogTitle">
+            ${element.blogtitle}
+        </p>
+        <p id="forBlogDescription">
+            ${element.blogdescription}
+        </p>
+
+        <button> delete </button> <button onclick="editDescription(${index})"> edit </button>
+    </div>`
+    }
+
+}
+function logOut() {
+    let confirmLogout = window.confirm('are you sure you want to logout?')
+    if (confirmLogout) {
+        localStorage.removeItem('augustJsUserIndex')
+        window.location.href = '../pages/login.html'
+    }
+
+
+}
+
+
+function editDescription(i) {
+    let editInput = prompt('edit blog description', `${blogDatabase[i].blogdescription}`)
+
+}
+
+
+
+
+// let myPromptValue = prompt('enter my name', 'users name')
+// console.log(myPromptValue);
+
+
+// let canLogout = window.confirm('are you sure?')
+// console.log(canLogout);
+
+
+
+// function return4(params) {
+//     let num = 'a' + 'b'
+//     console.log(num);
+//     return num
+// }
+
+// console.log(return4());
+
+// let newString = return4() + ' ' + 'hello'
+// console.log(newString);
